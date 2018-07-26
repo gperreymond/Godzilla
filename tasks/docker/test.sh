65eccd9fd192988c466bd84f7b188896233b1d53
Gilles Perreymond <gperreymond@gmail.com>
2018-07-26 18:33:14 +0200

    [travis] dgoss problem

--- tasks/docker/test.sh
+++ tasks/docker/test.sh
@@ -5,7 +5,7 @@ docker load --input /tmp/images/godzilla.tar
 export GOSS_FILES_STRATEGY=cp
 export GOSS_SLEEP=30
 
-curl -fsSL https://goss.rocks/install | sh
+curl -fsSL https://goss.rocks/install | sudo sh
 sleep 2s
 
-dgoss run -e ENABLE_DGOSS=1 -e NODE_ENV=test -e APP_RABBITMQ_USER=guest -e APP_RABBITMQ_PASS=guest gperreymond/godzilla
+dgoss run -e ENABLE_DGOSS=1 -e NODE_ENV=test -e APP_RABBITMQ_USER=${APP_RABBITMQ_USER} -e APP_RABBITMQ_PASS=${APP_RABBITMQ_PASS} gperreymond/godzilla
