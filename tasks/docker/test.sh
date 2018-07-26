b869dd151e744e3ec8e4fdaff85b83c2d270e95e
Gilles Perreymond <gperreymond@gmail.com>
2018-07-26 18:24:32 +0200

    [travis] try to dgoss

--- tasks/docker/test.sh
+++ tasks/docker/test.sh
@@ -8,4 +8,4 @@ export GOSS_SLEEP=30
 curl -fsSL https://goss.rocks/install | sh
 sleep 2s
 
-dgoss run -e ENABLE_DGOSS=1 gperreymond/godzilla
+dgoss run -e ENABLE_DGOSS=1 -e NODE_ENV=test -e APP_RABBITMQ_USER=guest -e APP_RABBITMQ_PASS=guest gperreymond/godzilla
